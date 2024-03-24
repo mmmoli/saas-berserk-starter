28b19e0b542993706cc15622bdcfdc446faa5fb3
Michele Memoli <michele.memoli@gmail.com>
2024-03-24 19:08:21 +0000

    feat(shared): updated theme

--- src/lib/ROUTES.ts
+++ src/lib/ROUTES.ts
@@ -9,7 +9,8 @@
  * PAGES
  */
 const PAGES = {
-  "/": `/`
+  "/": `/`,
+  "/app": `/app`
 }
 
 /**
@@ -133,7 +134,7 @@ export function route<T extends keyof AllTypes>(key: T, ...params: any[]): strin
 * ```
 */
 export type KIT_ROUTES = {
-  PAGES: { '/': never }
+  PAGES: { '/': never, '/app': never }
   SERVERS: Record<string, never>
   ACTIONS: Record<string, never>
   LINKS: Record<string, never>
---
+++ src/routes/(app)/app/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { Button } from '~ui/button';
+</script>
+
+<h1>App</h1>
+
+<Button>Click me</Button>
